b19b0a3a21b44132c2937fcee2fccca7083d5c6a
Carlos Galan Cladera <cgcladera@gmail.com>
2024-10-29 08:02:00 +0100

    feat(nvim): code completion

---
+++ nvim/.config/nvim/lua/config/cmp.lua
@@ -0,0 +1,133 @@
+local M = {}
+
+function M.setup()
+	local cmp_ok, cmp = pcall(require, "cmp")
+	if not cmp_ok then
+		return
+	end
+	local luasnip_ok, luasnip = pcall(require, "luasnip")
+	if luasnip_ok then
+		  require("luasnip/loaders/from_vscode").lazy_load()
+	end
+	
+ vim.api.nvim_set_hl(0, "CmpItemKindCopilot", { fg = "#6CC644" })
+ vim.api.nvim_set_hl(0, "CmpItemKindTabnine", { fg = "#CA42F0" })
+ vim.api.nvim_set_hl(0, "CmpItemKindEmoji", { fg = "#FDE030" })
+
+  local check_backspace = function()
+    local col = vim.fn.col "." - 1
+    return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
+  end
+
+  local icons = require "config.icons"
+  local sources = {
+      { name = "copilot" },
+      { name = "nvim_lsp" },
+      { name = "luasnip" },
+      { name = "cmp_tabnine" },
+      { name = "nvim_lua" },
+      { name = "buffer" },
+      { name = "path" },
+      { name = "calc" },
+      { name = "emoji" },
+  }
+  local config = {
+    snippet = {
+      expand = function(args)
+	if luasnip_ok then
+		luasnip.lsp_expand(args.body) -- For `luasnip` users.
+	end
+      end,
+    },
+    mapping = cmp.mapping.preset.insert {
+      ["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+      ["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+      ["<Down>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+      ["<Up>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+      ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+      ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+      ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+      ["<C-e>"] = cmp.mapping {
+        i = cmp.mapping.abort(),
+        c = cmp.mapping.close(),
+      },
+      -- Accept currently selected item. If none selected, `select` first item.
+      -- Set `select` to `false` to only confirm explicitly selected items.
+      ["<CR>"] = cmp.mapping.confirm { select = true },
+      ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif luasnip_ok and luasnip.expandable() then
+          luasnip.expand()
+        elseif luasnip_ok and luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+        elseif check_backspace() then
+          fallback()
+        else
+          fallback()
+        end
+      end, {
+        "i",
+        "s",
+      }),
+      ["<S-Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif luasnip_ok and luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end, {
+        "i",
+        "s",
+      }),
+    },
+    formatting = {
+      fields = { "kind", "abbr", "menu" },
+      format = function(entry, vim_item)
+        vim_item.kind = icons.kind[vim_item.kind]
+        vim_item.menu = ({
+          nvim_lsp = "",
+          nvim_lua = "",
+          luasnip = "",
+          buffer = "",
+          path = "",
+          emoji = "",
+        })[entry.source.name]
+
+        if entry.source.name == "emoji" then
+          vim_item.kind = icons.misc.Smiley
+          vim_item.kind_hl_group = "CmpItemKindEmoji"
+        end
+
+        if entry.source.name == "cmp_tabnine" then
+          vim_item.kind = icons.misc.Robot
+          vim_item.kind_hl_group = "CmpItemKindTabnine"
+        end
+
+        return vim_item
+      end,
+    },
+    sources = sources,
+    confirm_opts = {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = false,
+    },
+    window = {
+      completion = {
+        border = "rounded",
+        scrollbar = false,
+      },
+      documentation = {
+        border = "rounded",
+      },
+    },
+    experimental = {
+      ghost_text = false,
+    },
+  }
+  cmp.setup(config)
+end
+
+return M
--- nvim/.config/nvim/lua/plugins/intelligence.lua
+++ nvim/.config/nvim/lua/plugins/intelligence.lua
@@ -38,4 +38,49 @@ return {
 	    require('config.mason').setup()
     end,
   },
+-- Cmp
+	{
+		  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+  dependencies = {
+    {
+      "hrsh7th/cmp-nvim-lsp",
+      event = "InsertEnter",
+    },
+    {
+      "hrsh7th/cmp-emoji",
+      event = "InsertEnter",
+    },
+    {
+      "hrsh7th/cmp-buffer",
+      event = "InsertEnter",
+    },
+    {
+      "hrsh7th/cmp-path",
+      event = "InsertEnter",
+    },
+    {
+      "hrsh7th/cmp-cmdline",
+      event = "InsertEnter",
+    },
+    {
+      "saadparwaiz1/cmp_luasnip",
+      event = "InsertEnter",
+    },
+    {
+      "L3MON4D3/LuaSnip",
+      event = "InsertEnter",
+      dependencies = {
+        "rafamadriz/friendly-snippets",
+      },
+    },
+    {
+      "hrsh7th/cmp-nvim-lua",
+    },
+  },
+  config = function()
+	  require("config.cmp").setup()
+  end,
+
+	}
 }
