3973206ef65d225fa30822915c05b3f0ddbb3409
Carlos Galan Cladera <cgcladera@gmail.com>
2024-12-21 21:37:44 +0100

    feat(nvim): new keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,40 +1,39 @@
 local k = require("util.keymapping")
-local opts = { noremap = true, silent = true }
 
 -- Navigation
-k.map("n", "<leader>e", k.cmd("Oil"), opts)
+k.map("n", "<leader>e", k.cmd("Oil"))
 
 -- Buffers
-k.map("i", "jk", "<Esc>", opts)
-k.map("i", "jj", "<Esc>", opts)
-k.map("i", "kk", "<Esc>", opts)
-k.map("n", "<leader>w", k.cmd("w"), opts)
+k.map("i", "jk", "<Esc>")
+k.map("i", "jj", "<Esc>")
+k.map("i", "kk", "<Esc>")
+k.map("n", "<leader>w", k.cmd("w"))
+k.map("n", "<leader>wy", k.cmd('let @" = expand("%")'), { desc = "Yank buffer filepath" })
+k.map("n", "<leader>wY", k.cmd('let @* = expand("%")'), { desc = "Yank buffer filepath (clipboard)" })
 
 -- Windows
-k.map("n", "<C-l>", "<C-w>l", opts)
-k.map("n", "<C-h>", "<C-w>h", opts)
-k.map("n", "<C-k>", "<C-w>k", opts)
-k.map("n", "<C-j>", "<C-w>j", opts)
-k.map("n", "<M-Left>", k.cmd("vertical resize-2"), opts)
-k.map("n", "<M-Right>", k.cmd("vertical resize+2"), opts)
-k.map("n", "<M-Up>", k.cmd("horizontal resize-2"), opts)
-k.map("n", "<M-Down>", k.cmd("horizontal resize+2"), opts)
+k.map("n", "<C-l>", "<C-w>l")
+k.map("n", "<C-h>", "<C-w>h")
+k.map("n", "<C-k>", "<C-w>k")
+k.map("n", "<C-j>", "<C-w>j")
+k.map("n", "<M-Left>", k.cmd("vertical resize-2"))
+k.map("n", "<M-Right>", k.cmd("vertical resize+2"))
+k.map("n", "<M-Up>", k.cmd("horizontal resize-2"))
+k.map("n", "<M-Down>", k.cmd("horizontal resize+2"))
 
 -- Tabs
-k.map("n", "<C-M-l>", k.cmd("+tabnext"), {desc = "Next Tab"})
-k.map("n", "<C-M-h>", k.cmd("-tabnext"), {desc = "Previous Tab"})
-k.map("n", "<C-M-q>", k.cmd("tabc"), {desc = "Close Tab"})
+k.map("n", "<C-M-l>", k.cmd("+tabnext"), { desc = "Next Tab" })
+k.map("n", "<C-M-h>", k.cmd("-tabnext"), { desc = "Previous Tab" })
+k.map("n", "<C-M-q>", k.cmd("tabc"), { desc = "Close Tab" })
 
 -- Center scroll when searching
-k.map("n", "n", "nzz", opts)
-k.map("n", "N", "Nzz", opts)
-k.map("n", "*", "*zz", opts)
-k.map("n", "#", "#zz", opts)
-k.map("n", "g*", "g*zz", opts)
-k.map("n", "g#", "g#zz", opts)
-
+k.map("n", "n", "nzz")
+k.map("n", "N", "Nzz")
+k.map("n", "*", "*zz")
+k.map("n", "#", "#zz")
+k.map("n", "g*", "g*zz")
+k.map("n", "g#", "g#zz")
 
 -- Stay in indent mode
-k.map("v", "<", "<gv", opts)
-k.map("v", ">", ">gv", opts)
-
+k.map("v", "<", "<gv")
+k.map("v", ">", ">gv")
