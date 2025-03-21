af14d8876312a72dc200856dc0fffcc1755650cf
Carlos Galan Cladera <cgcladera@gmail.com>
2025-03-21 19:47:07 +0100

    feat(nvim): line and column highlights (#14)

--- nvim/.config/nvim/lua/config/colorscheme.lua
+++ nvim/.config/nvim/lua/config/colorscheme.lua
@@ -5,10 +5,11 @@ if catppuccin_ok then
 		flavour = "macchiato",
 		custom_highlights = function(colors)
 			return {
+				CursorColumn = { bg = colors.surface0 },
+				CursorLine = { bg = colors.surface0 },
+				CursorLineNr = { fg = colors.text, bg = colors.surface1 },
 				LineNr = { fg = colors.teal },
 				LineNrAbove = { fg = colors.peach },
-				CursorLineNr = { fg = colors.text, bg = colors.surface1 },
-				CursorLine = { bg = colors.surface1 },
 				NvimTreeWinSeparator = { bg = "none" },
 			}
 		end,
